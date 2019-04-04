14e3d23b7579690f454c8fbb01586780869ce8db
Joe Quattrone <joequattrone5@gmail.com>
2019-04-04 09:27:25 -0400

    Deleted fluff code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 
 class Hotel {
   constructor(hash) {
@@ -67,7 +55,6 @@ $(function() {
       headers: { 'Content-Type': 'application/json' }
     }).then(res => res.json())
       .then(function(json) {
-        document.querySelector("#hotel-container").innerHTML = ""
         json.forEach(function(hotel_data) {
         let hotel = new Hotel(hotel_data)
         hotel.render() })
